08042540eaa26a263cc7ca40a4feb71524a15dee
Jonathan Sterlin <jonathan.sterlin@gmail.com>
2013-03-26 03:48:14 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stblog::Application.config.secret_token = '8e65b5f4326f26d057a1b7eb0bd904fa301629a80b757f8651871eff50d36cd8998af55410cc340d6f2aaf8aea10e40f7c0978d611cac506c1eee9c70425ee46'
